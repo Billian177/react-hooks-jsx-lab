ecad4e0a69c273d6e4f57fae7c30bffd214307c3
bill apiyo <billian177@gmail.com>
2022-07-18 23:01:38 +0300

    commit tree 8753598c00c83cfcf568525053ca137f980c5ad0

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>One thing you are going to love about this guy is that everything you need is under one head.
+      Just access it with a line of code!
+    </p>
+    <img src={image} alt="I made this"/>
+    </div>;
 }
 
 export default About;
